e28a825116f7d3fd2937fb6774b384dd10f25a03
Marcos Rico <mrico@cps.edu>
2021-04-18 14:50:18 -0500

    Initial Commit

---
+++ Team7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Team7
+//
+//  Created by Amanda Brode on 4/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Team7/Team7App.swift
@@ -0,0 +1,17 @@
+//
+//  Team7App.swift
+//  Team7
+//
+//  Created by Amanda Brode on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Team7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
